b8233498d37cde4d9bd3b7b3d3bb77fb9e4def4f
Abdurizo <103021939+abdurizo@users.noreply.github.com>
2025-11-10 11:44:53 +0500

    validet form

--- src/app/modul/admin/teacher/add-edit-teacher/add-edit-teacher.component.ts
+++ src/app/modul/admin/teacher/add-edit-teacher/add-edit-teacher.component.ts
@@ -5,29 +5,38 @@ import { TeachersService } from '../services/teachers.service';
 @Component({
   selector: 'app-add-edit-teacher',
   templateUrl: './add-edit-teacher.component.html',
-  styleUrl: './add-edit-teacher.component.css'
+  styleUrl: './add-edit-teacher.component.css',
 })
 export class AddEditTeacherComponent {
-  form=this.fb.nonNullable.group({
-    name:['', Validators.required],
-    description:['', Validators.required],
-    addres:['', Validators.required],
-    dataOfBirth:['', Validators.required],
-    dataOfRegister:['', Validators.required],
-    phone:['', Validators.required],
-    email:['', Validators.required],
-    telegramUserName:['', Validators.required],
-    specialization:['', Validators.required],
-  })
+  form = this.fb.nonNullable.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    addres: ['', Validators.required],
+    dataOfBirth: ['', Validators.required],
+    dataOfRegister: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', Validators.required],
+    telegramUserName: ['', Validators.required],
+    specialization: ['', Validators.required],
+  });
   /**
-   * 
+   *
    */
-constructor(private fb: FormBuilder,private $teachers:TeachersService){}
-/**
- * 
- */
-add(){
-  const request = this.form.getRawValue();
-  this.$teachers.add(request).subscribe();
-}
+  constructor(private fb: FormBuilder, private $teachers: TeachersService) {}
+  /**
+   *
+   */
+  add() {
+    if (this.form.invalid) {
+      Object.values(this.form.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+      return;
+    }
+    const request = this.form.getRawValue();
+    this.$teachers.add(request).subscribe();
+  }
 }
